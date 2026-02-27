e296073ca85ad437fd617daea268cbb8891c837d
Florent Richard <florent.richard@supmaritime.fr>
2026-02-27 11:21:17 +0100

    Add a first joystick reading

--- src/Rudder.cpp
+++ src/Rudder.cpp
@@ -10,6 +10,8 @@ Rudder::Rudder()
   Init(0,0,0,0,0,0,0,0,0,0,{0,0},0,0);
   mDelta = 0;
   mT << 0, 0, 0;
+  mRrMax = 0;
+  mDeltaMax = 0;
 }
 
 void Rudder::Init(double aHr, double aAr, double aXpR, double aAh, double aTr, double aXpH, double aEpsilon, double aKappa, double aLpR, double aLambdaR, std::vector<double> aGammaR, double aRrMax, double aDeltaMax)
@@ -138,4 +140,4 @@ void Rudder::ComputeT(const Eigen::Vector3d& aMu, const double aRho, const sGeoP
 Eigen::Vector3d& Rudder::getT(void){return mT;}
 
 double Rudder::getDelta(void) const {return mDelta;}
-double Rudder::getDeltaMax(void){return mDeltaMax;}
+double Rudder::getDeltaMax(void) const {return mDeltaMax;}

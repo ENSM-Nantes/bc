872c451983dbb09432a60ac14d71ac5ebc70b1d6
florent richard <florent.richard@supmaritime.fr>
2026-03-30 15:19:07 +0200

    Remove delta

--- src/AIS.cpp
+++ src/AIS.cpp
@@ -65,7 +65,7 @@ std::vector<unsigned int> AIS::getReadyShips(void *aOtherShips, unsigned int now
   return readyShips;
 }
 
-std::tuple<std::string, int> AIS::generateClassAReport(void *aOtherShips, float aDeltaZ, float aDeltaX, void *aTerrain, unsigned long long aTimeStamp, unsigned int ship)
+std::tuple<std::string, int> AIS::generateClassAReport(void *aOtherShips, void *aTerrain, unsigned long long aTimeStamp, unsigned int ship)
 {  
   OtherShips *pOtherShips = (OtherShips*)aOtherShips;
   Terrain *pTerrain = (Terrain*)aTerrain;
@@ -99,8 +99,8 @@ std::tuple<std::string, int> AIS::generateClassAReport(void *aOtherShips, float 
 
   // BC internal coordinate system
 
-  float posX = pOtherShips->getPosition(ship).X + aDeltaX;
-  float posZ = pOtherShips->getPosition(ship).Z + aDeltaZ;
+  float posX = pOtherShips->getPosition(ship).X;
+  float posZ = pOtherShips->getPosition(ship).Z;
  
   float shipLong = pTerrain->xToLong(posX);
   float shipLat  = pTerrain->zToLat(posZ);

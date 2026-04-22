6c1b321064f08136e54d18312d4b78761e5bf4ee
florent richard <florent.richard@supmaritime.fr>
2026-04-22 15:27:29 +0200

    Process roll angle

--- src/ShipGlobalParams.hpp
+++ src/ShipGlobalParams.hpp
@@ -8,9 +8,12 @@ struct sGeoParams{
   double lPP; //length  
   double b; //breadth
   double d; //draught
-  double volume; //subwater volume 
+  double volume; //subwater volume
+  double zG; //Altitudinal coordinate of center of gravity of ship
   double xG; //Longitudinal coordinate of center of gravity of ship
   double cB; //Coefficient Block
+  double kM; //Distance between kheel to metacentric 
+  double gM; //Distance between gravity center to metacentric
   double propSpacing; //length between 2 propeller
   double rudSpacing; //length between 2 rudder  
 };
